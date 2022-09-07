0587c7bea95a44e973d5e8a5973eee2541704396
alex <alexanderscottmiller@gmail.com>
2022-09-07 15:17:19 -0400

    code: main template

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,18 @@
-use clap::Parser;
+// use clap::Parser;
 
 /// Simple program to greet a person
-#[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
-struct Args {
-    /// The name of the file to process
-    #[clap(short, long, value_parser)]
-    file: String,
-
-    // TODO: Deal Configuration
-}
-
-#der
-struct DealConfiguration {
-
-}
+// #[derive(Parser, Debug)]
+// #[clap(author, version, about, long_about = None)]
+// struct Args {
+//     /// The name of the file to process
+//     #[clap(short, long, value_parser)]
+//     file: String,
+//
+//     // TODO: Deal Configuration
+// }
 
 fn main() {
-    let args = Args::parse();
+    // let args = Args::parse();
 
     // Configure a deal for the file
 }
