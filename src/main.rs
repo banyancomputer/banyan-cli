d6af958dc3453aeb571c6f34501c313ebc50edce
alex <alexanderscottmiller@gmail.com>
2022-09-07 14:26:39 -0400

    ci: implement code coverage + ci

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// The name of the file to process
+    #[clap(short, long, value_parser)]
+    file: String,
+
+    // TODO: Deal Configuration
+}
+
+#der
+struct DealConfiguration {
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    // Configure a deal for the file
 }
