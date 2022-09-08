9d232aa703b8b6a4a37d9ae8f15e8440abbf61e0
alex <alexanderscottmiller@gmail.com>
2022-09-07 22:56:22 -0400

    code: impl cli function routing

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,23 @@ use args::BanyanArgs;
 // use deal_maker::DealMaker;
 
 fn main() {
+    // Parse the CLI arguments
     let args: BanyanArgs = BanyanArgs::parse();
-    println!("Args: {:#?}", args);
+    // Route the CLI arguments to the appropriate function
+    match args.command_type {
+        args::CommandType::Deal(deal_command) => {
+            match deal_command.command {
+                args::DealSubcommand::Submit(submit_deal) => {
+                    println!("Submitting a new deal for file: {}", submit_deal.file);
+                },
+                args::DealSubcommand::Show => {
+                    println!("Showings all deals");
+                },
+            }
+        },
+        args::CommandType::Config(config_command) => {
+            println!("Configuring the Banyan CLI");
+        },
+    }
     return;
 }
