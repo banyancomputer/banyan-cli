b45dfed357d5f9d5e7ab2afe4cb726e64c19e143
alex <alexanderscottmiller@gmail.com>
2022-09-14 13:18:00 -0400

    Saving work

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,15 @@
-mod deal_maker;
+mod client;
 mod args;
+mod hasher;
 
 use clap::Parser;
 use args::BanyanArgs;
 use client::BanyanClient;
-// use deal_maker::DealMaker;
 
 fn main() {
     // Parse the CLI arguments
     let args: BanyanArgs = BanyanArgs::parse();
-    // Initialize the Client
-
-    let client = BanyanClient::builder()
-        .banyan_contract_address(args.banyan_contract_address)
-        .eth_api_url(args.eth_api_url)
-        .eth_api_key(args.eth_api_key)
-        .eth_private_key(args.eth_private_key)
-        .estuary_api_hostname(args.estuary_api_hostname)
-        .estuary_api_key(args.estuary_api_key)
-        .build()
-        .unwrap();
+    dbg!(args);
     // Route the CLI arguments to the appropriate function
     match args.command_type {
         args::CommandType::Deal(deal_command) => {
