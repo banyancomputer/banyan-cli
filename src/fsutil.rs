8b6f9ea5b50dbd925d2716316a957f551da1d1a1
Claudia Richoux <c@laudiacay.cool>
2023-01-11 23:26:11 -0800

    broken agaain

--- src/fsutil.rs
+++ src/fsutil.rs
@@ -2,7 +2,7 @@ use crate::fs_iterator::FilesystemIterator;
 use anyhow::{anyhow, Result};
 use tokio_stream::{Stream, StreamExt, StreamMap};
 
-
+use async_recursion::async_recursion;
 use std::path::{Path, PathBuf};
 
 pub fn ensure_path_exists_and_is_dir(path: &Path) -> Result<()> {
@@ -24,33 +24,90 @@ pub fn ensure_path_exists_and_is_empty_dir(path: &Path) -> Result<()> {
     Ok(())
 }
 
+#[async_recursion]
 async fn copy_file_or_dir(from: PathBuf, to_root: PathBuf) -> Result<PathBuf> {
     let new_path = to_root.join(from.strip_prefix("/")?);
     if from.is_dir() {
         tokio::fs::create_dir_all(&new_path).await?;
+    } else if from.is_symlink() {
+        // follow the symlink and copy what's there
+        let target = std::fs::read_link(&from)?;
+        copy_file_or_dir(target, to_root).await?;
     } else {
         tokio::fs::copy(from, &new_path).await?;
     }
     Ok(new_path)
 }
 
-// TODO make a nice generalized function. make an iterable trait i guess.
-async fn copy_paths_recursively(
+pub(crate) async fn copy_paths_recursively(
     paths: Vec<PathBuf>,
     scratch_root: PathBuf,
     follow_symlinks: bool,
 ) -> impl Stream<Item = (String, Result<PathBuf>)> + Unpin {
-    // TODO do the multiplexing correctly
     let mut map = StreamMap::new();
     for path in paths {
         let fsi = FilesystemIterator::new(path.clone(), follow_symlinks).await;
+        // TODO sucks why are you cloning twice
         let scratch_root = scratch_root.clone();
         let copy_file = Box::pin(fsi.then(move |res| copy_file_or_dir(res, scratch_root.clone())));
         let path_string = path.to_string_lossy().to_string();
-        map.insert(
-            path_string,
-            copy_file
-        );
+        map.insert(path_string, copy_file);
     }
     map
+}
+
+// this comment lies in memoriam of the time i set these both to 10. if you estimate the disk
+// space used by a directory as only 512 bits, this would have filled 5 terabytes of disk space.
+// i'm not sure what i was thinking.
+pub fn make_big_filesystem_clusterfuck(depth_to_go: usize, width: usize, cwd: PathBuf) {
+    if depth_to_go == 0 {
+        for i in 0..width {
+            let mut path = cwd.clone();
+            path.push(format!("file{i}"));
+            std::fs::File::create(path).unwrap();
+        }
+    } else {
+        for i in 0..width {
+            let mut path = cwd.clone();
+            path.push(format!("dir{i}"));
+            std::fs::create_dir(path.clone()).unwrap();
+            make_big_filesystem_clusterfuck(depth_to_go - 1, width, path);
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    #[tokio::test]
+    async fn test_copy_paths_recursively() {
+        use super::*;
+        let tmp = tempfile::tempdir().unwrap();
+        let scratch_root = tmp.path().join("scratch");
+        let og_root = tmp.path().join("og");
+        std::fs::create_dir(&scratch_root).unwrap();
+        std::fs::create_dir(&og_root).unwrap();
+        make_big_filesystem_clusterfuck(3, 3, og_root.clone());
+        let paths = std::fs::read_dir(og_root.clone())
+            .unwrap()
+            .map(|res| res.unwrap().path())
+            .collect::<Vec<PathBuf>>();
+
+        let stream = copy_paths_recursively(paths, scratch_root.clone(), false).await;
+        let out_files = stream
+            .map(|res| res.1.unwrap())
+            .collect::<Vec<PathBuf>>()
+            .await;
+
+        let in_files = FilesystemIterator::new(og_root, false)
+            .await
+            .collect::<Vec<PathBuf>>()
+            .await;
+
+        assert_eq!(in_files.len() - 1, out_files.len());
+        for file in out_files {
+            let stripped =
+                Path::new("/").join(file.strip_prefix(&scratch_root).unwrap().to_path_buf());
+            assert!(in_files.contains(&stripped));
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
 mod args;
 mod fs_iterator;
+mod fsencryption;
 mod fsutil;
 
 use clap::Parser;
-
-
-
+use futures::FutureExt;
+use tokio_stream::{StreamExt, StreamMap};
 
 //use iroh_car::{CarWriter};
 //use iroh_unixfs::builder::Config;
 //use iroh_unixfs::chunker::ChunkerConfig;
-
 
 //use crate::fs_iterator::do_singlethreaded_test;
 
@@ -28,7 +27,21 @@ async fn main() {
 
     // copy all the files over to an encrypted scratch directory
     let scratch_dir = args.output_dir.join("scratch");
-    std::fs::create_dir(scratch_dir).expect("could not create scratch directory");
+    std::fs::create_dir(&scratch_dir).expect("could not create scratch directory");
 
     // copy from inputs to scratch dir
+    let copy_stream =
+        fsutil::copy_paths_recursively(args.input, scratch_dir.clone(), args.follow_symlinks).await;
+
+    copy_stream
+        .then(|(_, file)| {
+            let file = file.unwrap();
+            fsencryption::partition_file(file).map(|x| (file, x))
+        })
+        .fold(StreamMap::new(), |mut acc, (file, stream)| {
+            acc.insert(file, stream);
+            acc
+        }).await
+        .then(|(_, file)| fsencryption::encrypt_file_in_place(file.unwrap()))
+        .await;
 }
