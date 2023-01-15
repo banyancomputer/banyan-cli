b51aa8e981c29b67d1e50c8cf4d5574cb539c8a1
Claudia Richoux <c@laudiacay.cool>
2023-01-15 02:33:07 -0600

    significant refactor of iterator and everything, thanks jwalk. deduplication is done. partitioning is done. copying is done. encryption is done.

---
+++ src/fs_carfiler.rs
@@ -0,0 +1,5 @@
+// ok so we wanna make a car file
+// i think i'm willing to bet that the way people wanna make a car file is "put as large a subtree as you can into each one"
+// so we're gonna start at the leftmost node. we're gonna try and get as many of its siblings as we can.
+// if we succeed we're gonna go up a node. and get as many of its siblings as we can.
+// this is... hard.
