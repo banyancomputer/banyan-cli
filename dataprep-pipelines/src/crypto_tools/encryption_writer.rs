964d2c2ae3ffe7c60741825956568f560c948037
Claudia Richoux <c@laudiacay.cool>
2023-02-01 15:27:09 -0500

    yeehaw

--- dataprep-pipelines/src/crypto_tools/encryption_writer.rs
+++ dataprep-pipelines/src/crypto_tools/encryption_writer.rs
@@ -1,4 +1,4 @@
-use crate::crypto_tools::key_and_nonce_types::{keygen, KeyAndNonce, KeyAndNonceToDisk};
+use crate::crypto_tools::key_and_nonce_types::{keygen, KeyAndNonce, KeyAndNonceToDisk, TAG_SIZE, BUF_SIZE};
 use aead::stream::NewStream;
 use aead::stream::{Encryptor, StreamBE32, StreamPrimitive};
 
@@ -10,10 +10,8 @@ use futures::executor;
 use std::cell::RefCell;
 use std::io::prelude::Write;
 use std::pin::Pin;
-use tokio::io::{AsyncWrite, AsyncWriteExt};
-
-const MAX_SAFE_ENCRYPTION_SIZE: usize = 34_359_738_368; // 32 gigs, the GCM safe limit
-const BUF_SIZE: usize = 1024 * 1024; // 1 MB
+use aead::Payload;
+use tokio::io::{AsyncSeek, AsyncWrite, AsyncWriteExt};
 
 /// A wrapper around a writer that encrypts the data as it is written.
 /// Should not be used on files larger than 32 GB.
@@ -42,6 +40,10 @@ impl<W: AsyncWrite + Unpin> EncryptionWriter<W> {
         // keygen
         let keygen @ KeyAndNonce { key, nonce } = keygen();
 
+        // write space for the tag to the start of the file
+        // TODO (laudiacay): this is a hack, fix it
+        let mut writer = writer;
+
         // Create the encryptor.
         let cipher = Aes256Gcm::new(&key);
         let encryptor = RefCell::new(StreamBE32::from_aead(cipher, &nonce).encryptor());
@@ -65,7 +67,7 @@ impl<W: AsyncWrite + Unpin> EncryptionWriter<W> {
         // TODO (laudiacay): check this logic better, especially once your PR is merged on rust crypto
         self.encryptor
             .into_inner()
-            .encrypt_last_in_place(b"".as_ref(), &mut *self.buf.borrow_mut())
+            .encrypt_last(Payload::from(self.buf.borrow_mut().as_slice()))
             .unwrap();
         executor::block_on(self.writer.borrow_mut().write_all(&self.buf.borrow_mut()))?;
         *self.bytes_written.borrow_mut() += self.buf.borrow().len();
@@ -78,7 +80,7 @@ impl<W: AsyncWrite + Unpin> EncryptionWriter<W> {
 }
 
 /// Implement the Write trait for EncryptionWriter.
-impl<W: AsyncWrite + Unpin> Write for EncryptionWriter<W> {
+impl<W: AsyncWrite + Unpin > Write for EncryptionWriter<W> {
     /// Write data to the buffer
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
         let mut self_pin = Pin::new(&mut *self);
@@ -137,17 +139,11 @@ impl<W: AsyncWrite + Unpin> Write for EncryptionWriter<W> {
         // flush underlying writer to wherever it's going i guess
         executor::block_on(self_pin.writer.borrow_mut().flush())?;
 
-        // update counter for how many bytes you wrote, check for safe GCM usage limits
+        // update counter for how many bytes you wrote
         *self_pin.bytes_written.borrow_mut() += self_pin.buf.borrow().len();
-        if *self.bytes_written.borrow() >= MAX_SAFE_ENCRYPTION_SIZE {
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::FileTooLarge,
-                "File too large to encrypt",
-            ));
-        };
 
         // clear out the buffer
-        self.buf.borrow_mut().clear();
+        self_pin.buf.borrow_mut().clear();
         Ok(())
     }
 }
