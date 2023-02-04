99fa5e718ef9e4341eede8c6c8d41837fed875a0
Claudia Richoux <c@laudiacay.cool>
2023-02-04 13:49:46 -0500

    removing some things

--- dataprep-pipelines/src/lib.rs
+++ dataprep-pipelines/src/lib.rs
@@ -5,15 +5,9 @@
 /* General Project Chores */
 // TODO (xBalbinus & thea-exe): Handle panics appropriately/get rid of all the unwraps
 // TODO (xBalbinus & thea-exe): get rid of all the clones and stop copying around pathbufs
-// TODO (xBalbinus & thea-exe): generally clean up imports and naming. the fs_yadayadayada stuff is particularly bad.
-
-/* Hardcore project TODOs before mvp */
-// TODO (laudiacay): We can implement the pipeline with a single FS read maybe. Look into this. Be sure to tally up the reads before attempting this.
-// TODO (laudiacay): Encrypt filenames and other metadata. Need to hide directory structure.
 
 /* Speculative Lifts */
 // TODO (laudiacay): Can / Should we include an option to pack chunks into a CAR file? Look into this.
-// TODO (laudiacay): What if we tried encrypting the file in place with one file handle. Look into this.
 // TODO (laudiacay) : Handle pinning threads to CPU cores (with tokio localsets and runtimes?) correctly so that disk throughput is maximized
 
 /* Dataprep:
@@ -24,7 +18,7 @@
  *      - original file path to random chunk name / path
  *      - random chunk paths point to the key-path used to encrypt the chunk.
  *      - keys stored in csv file
- * 5. TODO (laudiacay): Encyprpt the manifest file in place with some master key.
+ * 5. Encyprpt the manifest file in place with some master key. (later, optional)
  * 6. Use manifest file to repopulate the original directory structure
  * 7. TODO (laudiacay): Make car file with it.
  */
