a4818f6ff314e09d227553668c49a3256a3775c0
amiller68 <alexanderscottmiller@gmail.com>
2023-02-06 17:09:04 -0500

    feat: simple test set up with depth, width, and size

--- dataprep-pipelines/tests/integration.rs
+++ dataprep-pipelines/tests/integration.rs
@@ -5,8 +5,10 @@ extern crate rand;
 use dir_assert::assert_paths;
 use rand::Rng;
 use std::path::PathBuf;
-use tokio::fs;
-use tokio::io::AsyncWriteExt;
+use std::{
+    io::{Write},
+    fs
+};
 
 lazy_static! {
     static ref MANIFEST_FILE: PathBuf = PathBuf::from("test/manifest.json");
@@ -16,16 +18,16 @@ lazy_static! {
     static ref UNPACKED_DIR: PathBuf = PathBuf::from("test/unpacked");
 }
 
-async fn setup_structure() {
+fn setup_structure() {
     // remove any old test crud
-    fs::remove_dir_all(&*TEST_DIR).await.unwrap();
-    fs::create_dir(&*TEST_DIR).await.unwrap();
+    fs::remove_dir_all(&*TEST_DIR).unwrap();
+    fs::create_dir(&*TEST_DIR).unwrap();
     // create input directory
-    fs::create_dir(&*INPUT_DIR).await.unwrap();
+    fs::create_dir(&*INPUT_DIR).unwrap();
     // create output directory
-    fs::create_dir(&*OUTPUT_DIR).await.unwrap();
+    fs::create_dir(&*OUTPUT_DIR).unwrap();
     // create final output directory for unpacked
-    fs::create_dir(&*UNPACKED_DIR).await.unwrap();
+    fs::create_dir(&*UNPACKED_DIR).unwrap();
 }
 
 async fn transform_and_check() {
@@ -52,29 +54,98 @@ async fn transform_and_check() {
     assert_paths(&*INPUT_DIR, &*UNPACKED_DIR).unwrap();
 }
 
-// TODO: make a function that puts random data into a file with a given length
-// so the argumetns are like (path, length) and it reutrns nothign
-
-async fn create_directory_structure(path: &str, length: usize) {
-    // creating a directory at the given path
-    fs::create_dir(path).await.unwrap();
-    // creates length number of directories and files within the directory at the given path
-    for i in 0..length {
-        // new directory path made
-        let dir_path = format!("{}/{}", path, i);
-        // create directory in the new directory path
-        fs::create_dir(dir_path.clone()).await.unwrap();
-        // new file path made
-        let file_path = format!("{}/file{}", dir_path, i);
-        // file is then created in the directory
-        let mut file = fs::File::create(file_path).await.unwrap();
-        // random data
-        let random_data = rand::thread_rng().gen::<[u8; 32]>();
-        file.write_all(&random_data);
+// TODO (thea-exe): Add a quick test for this
+/// Create a random file with a given size
+pub fn create_random_file(path: PathBuf, size: usize) {
+    let mut file = fs::File::create(path).unwrap();
+    let mut rng = rand::thread_rng();
+    for _ in 0..size {
+        file.write(&[rng.gen()]).unwrap();
     }
 }
-#[tokio::test]
-async fn test_create_directory_structure() {
-    // assert and check the created directory structure
-    create_directory_structure("test_directory", 10);
+
+/// Everything is a file in Unix :) including directories
+/// Struct for representing a file structure, regardless of depth (i.e. a file or a directory)
+pub struct FileStructure {
+    /// How many files should be in the file (if it has depth > 0)
+    pub width: usize,
+    /// How deep the directory structure should be. 0 means this is a file
+    pub depth: usize,
+    /// How much data should be in the file
+    pub target_size: usize
+}
+
+// TODO (amiller68) : maybe benchmark
+// TODO (amiller68) : Can we use stream iterators here to improve performance?
+impl FileStructure {
+    /// Create a new FileStructure
+    /// width: Desired width of the file structure
+    /// depth: Desired depth of the file structure
+    /// target_size: Desired size of the file structure
+    pub fn new(width: usize, depth: usize, target_size: usize) -> Self {
+        Self {
+            width,
+            depth,
+            target_size,
+        }
+    }
+
+    /// Generate a balanced FileStructure with the given path
+    /// This means that the number of files in each directory is the same
+    /// Only leaf directories contain files
+    /// # Arguments
+    /// * `path` - The path to generate the file structure at
+    /// # Panics
+    /// Panics if the path already exists
+    pub fn generate_balanced(&self, path: PathBuf) {
+        // Panic if the path already exists. We don't want to overwrite anything!
+        assert!(!path.exists());
+
+        // If this is 0, we're creating a file
+        if self.depth == 0 {
+            // Create a file with the target size
+            create_random_file(path, self.target_size);
+        }
+        // Otherwise, we're creating a directory
+        else {
+            // Create a directory at the given path
+            fs::create_dir(&path).unwrap();
+            for i in 0..self.width {
+                // Read a fixed amount of data from target size
+                let target_size = self.target_size / self.width;
+                // Push the new path onto the path
+                let mut new_path = path.clone();
+                new_path.push(i.to_string());
+                // TODO: Is it ok to recurse here?
+                // Generate a new FileStructure with the new path
+                FileStructure::new(self.width, self.depth - 1, target_size).generate_balanced(new_path);
+            }
+        }
+    }
+
+    // TODO (thea-exe) : Can we introduce randomness here?
+    /// Generate a random FileStructure with the given path
+    /// This means that the number of files in each directory is not the same
+    /// The Size of each file is not the same
+    /// Any directory can contain files
+    /// # Arguments
+    /// * `path` - The path to generate the file structure at
+    /// # Panics
+    /// Panics if the path already exists
+    pub fn generate_random(&self, path: PathBuf) {
+        // Panic if the path already exists. We don't want to overwrite anything!
+        assert!(!path.exists());
+        todo!("Implement me!");
+    }
+}
+
+#[test]
+fn test_create_directory_structure() {
+    // create a directory structure with a width of 2, depth of 2, and a target size of 100 bytes
+    let file_structure = FileStructure::new(2, 2, 1024 * 1024);
+    // create a directory structure at the given path
+    file_structure.generate_balanced(PathBuf::from("test_set"));
+
+    // TODO (thea-exe): Is this the right / expected size?
+    // TODO (thea-exe): Does this break with certain descriptors (e.g. 0 width, 0 depth, etc.)
 }
