592052592d1a08ca348b99f82fb1622496b23667
Vera Gonzalez <me@vera.lgbt>
2023-06-06 16:26:39 -0400

    feat: added wasm testing suite

--- tomb-wasm/src/utils/metadata.rs
+++ tomb-wasm/src/utils/metadata.rs
@@ -6,7 +6,7 @@ use wnfs::{
 };
 
 #[wasm_bindgen]
-pub async fn send_string(value: String) -> Result<String, JsError> {
+pub async fn save_metadata(value: String) -> Result<String, JsError> {
     let store: &NetworkBlockStore = &NetworkBlockStore::new("http://127.0.0.1", 5001);
     let cid = store
         .put_block(value.as_bytes().to_vec(), IpldCodec::Raw)
@@ -16,12 +16,20 @@ pub async fn send_string(value: String) -> Result<String, JsError> {
 }
 
 #[wasm_bindgen]
-pub async fn retrieve_string(cid: String) -> Result<String, JsError> {
+pub async fn load_metadata(cid: String) -> Result<String, JsError> {
     let store: &NetworkBlockStore = &NetworkBlockStore::new("http://127.0.0.1", 5001);
     let bytes = store.get_block(&Cid::try_from(cid)?).await.unwrap();
     let value: String = std::str::from_utf8(&bytes)?.to_string();
     Ok(value)
 }
 
-#[wasm_bindgen]
-pub async fn pack() {}
+#[cfg(test)]
+mod tests {
+    use wasm_bindgen_test::*;
+    
+    #[wasm_bindgen_test]
+    fn simple() {
+        assert!(true);
+    }
+}
+
