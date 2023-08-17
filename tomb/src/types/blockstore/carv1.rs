024b81a0612f172dd3d176927c441582e8cc22fd
Vera Gonzalez <me@vera.lgbt>
2023-08-17 16:27:42 -0400

    fixed header test for carv1

--- tomb/src/types/blockstore/carv1.rs
+++ tomb/src/types/blockstore/carv1.rs
@@ -151,7 +151,7 @@ mod test {
     use super::BlockStore;
     use anyhow::Result;
     use serial_test::serial;
-    use std::{fs::remove_file, path::Path, str::FromStr};
+    use std::{fs::{remove_file, create_dir_all}, path::Path, str::FromStr};
     use tomb_common::{types::blockstore::tombblockstore::TombBlockStore, utils::test::car_setup};
     use wnfs::{
         common::BlockStore as WnfsBlockStore,
@@ -268,9 +268,12 @@ mod test {
     #[tokio::test]
     #[serial]
     async fn from_scratch() -> Result<()> {
-        let original_path = &Path::new("test")
-            .join("car")
-            .join("carv1_blockstore_from_scratch.car");
+        let dir = &Path::new("test").join("car");
+        if !dir.exists() {
+            create_dir_all(dir)?;
+        }
+        let original_path = &dir.join("carv1_blockstore_from_scratch.car");
+        
         // Remove it if its still there from previous test
         if original_path.exists() {
             remove_file(original_path)?;
