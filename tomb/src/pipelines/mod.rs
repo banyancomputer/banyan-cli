a621248c80fed342688b6b37c26f147d42570467
Vera Gonzalez <me@vera.lgbt>
2023-08-17 13:19:51 -0400

    add more debut to unpack

--- tomb/src/pipelines/mod.rs
+++ tomb/src/pipelines/mod.rs
@@ -85,21 +85,23 @@ mod test {
     #[serial]
     async fn unpack() -> Result<()> {
         let test_name = "unpack";
+        println!("doing test setup");
         // Create the setup conditions
         let origin = &test_setup(test_name).await?;
+        println!("finished test setup; doing init");
         // Initialize
         configure::init(origin).await?;
+        println!("finished init; doing pack");
         // Pack locally
         pack::pipeline(origin, true).await?;
+        println!("finished pack; doing unpack setup");
         // Create a new dir to unpack in
         let unpacked_dir = &origin
             .parent()
             .expect("origin has no parent")
             .join(format!("{}_unpacked", test_name));
         create_dir_all(unpacked_dir)?;
-
         println!("starting unpacking");
-
         // Run the unpacking pipeline
         unpack::pipeline(origin, unpacked_dir).await?;
         println!("finished unpacking");
