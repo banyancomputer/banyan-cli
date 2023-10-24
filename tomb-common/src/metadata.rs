8208d3b82e9bf45cfbc970142e580966a473c0c0
amiller68 <alexanderscottmiller@gmail.com>
2023-10-23 23:12:21 -0400

    fix: found a bug

--- tomb-common/src/metadata.rs
+++ tomb-common/src/metadata.rs
@@ -399,25 +399,39 @@ impl FsMetadata {
         Ok(transformed_entries)
     }
 
-    /// Mv a node to a new location
+    /// Mv a file or directory to a new location
     pub async fn mv(
         &mut self,
         src_path_segments: &[String],
         dest_path_segments: &[String],
-        content_store: &impl RootedBlockStore,
+        store: &impl RootedBlockStore,
     ) -> Result<()> {
-        self.root_dir
-            .basic_mv(
-                src_path_segments,
-                dest_path_segments,
-                true,
-                Utc::now(),
-                &mut self.forest,
-                content_store,
-                &mut thread_rng(),
+        // Search through the PrivateDirectory for a Node that matches the path provided
+        let result = self
+            .root_dir
+            .get_node(&src_path_segments, true, &self.forest, store)
+            .await;
+
+        if let Ok(node) = result && node.is_some() {
+            // Create the subdirectory
+            self.root_dir
+                .basic_mv(
+                    &src_path_segments,
+                    &dest_path_segments,
+                    true,
+                    Utc::now(),
+                    &mut self.forest,
+                    store,
+                    &mut thread_rng(),
+                )
+                .await?;
+        } else {
+            return Err(SerialError::NodeNotFound(
+                src_path_segments.join("/").to_string(),
             )
-            .await
-            .map_err(|_| SerialError::NodeNotFound(src_path_segments.join("/")).into())
+            .into());
+        }
+        Ok(())
     }
 
     /// Cp a file to a new location while deduplicating
@@ -747,6 +761,63 @@ mod test {
             .await?;
         assert_eq!(kitty_bytes, new_kitty_bytes);
 
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn add_mkdir_mv() -> Result<()> {
+        let metadata_store = MemoryBlockStore::default();
+        let content_store = MemoryBlockStore::default();
+        let wrapping_key = &EcEncryptionKey::generate().await?;
+
+        let mut fs_metadata =
+            _init_save_unlock(wrapping_key, &metadata_store, &content_store).await?;
+
+        // Add a new file
+        let file_path = vec!["file".to_string()];
+        let content = "loop de doop de dah".as_bytes().to_vec(); 
+        fs_metadata
+            .write(
+                &file_path,
+                &metadata_store,
+                &content_store,
+                content.clone(),
+            )
+            .await?;
+
+        
+        println!("file_path: {:?}", file_path);
+        // Add a new dir
+        let dir_path = vec!["dir".to_string()];
+        fs_metadata.mkdir(&dir_path, &metadata_store).await?;
+        let new_dir = fs_metadata
+            .get_node(&dir_path, &metadata_store)
+            .await?
+            .expect("dir not found");
+        match new_dir {
+            PrivateNode::Dir(dir) => dir,
+            _ => panic!("dir not found"),
+        };
+        println!("dir_path: {:?}", dir_path);
+        // Move File into the dir
+        let mv_file_path = vec!["dir".to_string(), "file".to_string()];
+        fs_metadata
+            .mv(&file_path, &mv_file_path, &metadata_store)
+            .await?;
+        let new_file = fs_metadata
+            .get_node(&mv_file_path, &metadata_store)
+            .await?
+            .expect("file not found");
+        match new_file {
+            PrivateNode::File(file) => file,
+            _ => panic!("file not found"),
+        };
+        println!("mv_file_path: {:?}", mv_file_path);
+        // Read the file
+        let new_file_content = fs_metadata
+            .read(&mv_file_path, &metadata_store, &content_store)
+            .await?;
+        assert_eq!(content, new_file_content);
         Ok(())
     }
 
